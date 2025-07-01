77f6788ab2f924108b717fdfa61dab3f51775a27
divya30-10 <nageshmahalgi65@gmail.com>
2025-07-01 18:33:29 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,38 @@
+# creating a list  
+list_of_fruits = ['apple', 'mango', 'banana', 'orange', 'guava']  
+  
+# printing the list  
+print("List of Fruits:", list_of_fruits)  
+  
+# using the append() method  
+list_of_fruits.append('grapes')  
+  
+# printing the updated list  
+print("Updated List of Fruits:", list_of_fruits)
+
+# initializing the lists
+shopping_list = ['apple', 'mango', 'banana']
+new_items = ['orange', 'guava', 'grapes']
+
+# printing the lists
+print("Old Shopping List:", shopping_list)
+print("New Items:", new_items)
+
+# using the extend() method
+shopping_list.extend(new_items)
+
+# printing the updated list
+print("Updated Shopping List:", shopping_list)
+
+# initializing the list
+shopping_list = ['apple', 'mango', 'banana', 'grapes']
+
+# printing the list
+print("Old Shopping List:", shopping_list)
+
+# using the insert() method
+shopping_list.insert(2, 'strawberry')
+
+# printing the updated list
+print("New Shopping List:", shopping_list)
+
